29e82ef1dc9ae647468583dd41b101d0493230b2
ahmed sekou magassouba <ahmed.magassouba@laplateforme.io>
2022-06-03 20:57:59 +0200

    todolist

--- todolist.php
+++ todolist.php
@@ -10,12 +10,63 @@
     <!-- lien css -->
     <link rel="stylesheet" href="Css/todolist.css">
 
+    <!-- lien UNICONS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
     <!-- lien script js -->
     <script src="Js/todolist.js"></script>
 </head>
 
 <body>
+    <!-- ========================== HEADER ========================= -->
 
+    <header>
+
+    </header>
+
+
+    <!-- ========================== Main ========================= -->
+    <main>
+
+        <!-- ======= formulaire ======= -->
+        <form>
+            <input type="text" name="text" id="text" placeholder="Ajouter une tache..." minlength="5">
+            <input type="submit" value="Ajouter" id="add">
+        </form>
+
+        <!-- ======= liste des taches ======= -->
+        <div class="container">
+
+            <!-- ======= section des nouvelles taches ======= -->
+            <section class="newTask">
+                <h3>Nouvelle tâche</h3>
+                <ul>
+                </ul>
+            </section>
+
+
+            <!-- ======= section des taches non complete ======= -->
+            <section class="taskNotComp">
+                <h3>Tâche en cours</h3>
+                <ul>
+                </ul>
+            </section>
+
+            <!-- ======= section des taches complete ======= -->
+            <section class="taskComp">
+                <h3>Tâche complète</h3>
+                <ul>
+                </ul>
+            </section>
+
+        </div>
+
+    </main>
+
+    <!-- ==========================FOOTER========================= -->
+    <footer>
+
+    </footer>
 </body>
 
 </html>
